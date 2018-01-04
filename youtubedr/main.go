52eab32943fd3db5886ffa4c53618ea215ccc8c0
丛前有只兔子 <huoda.china@163.com>
2018-01-04 17:43:05 +0800

    Update main.go

--- youtubedr/main.go
+++ youtubedr/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"path/filepath"
 
-	// 	. "github.com/SegueII/youtube"
+	. "github.com/SegueII/youtube"
 )
 
 func main() {
@@ -14,8 +14,8 @@ func main() {
 	log.Println(flag.Args())
 	currentDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	log.Println("download to dir=", currentDir)
-	// 	y := NewYoutube(true)
-	// 	arg := flag.Arg(0)
-	// 	y.DecodeURL(arg)
+	y := NewYoutube(true)
+	arg := flag.Arg(0)
+	y.DecodeURL(arg)
 	// 	y.StartDownload(currentDir)
 }
